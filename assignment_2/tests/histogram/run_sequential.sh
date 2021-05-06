6516b7e920606613e3ee27769488cd04b5cd33a0
Tony Wang <tonywang008@gmail.com>
2021-05-06 23:03:50 +0200

    Added Tests

---
+++ assignment_2/tests/histogram/run_sequential.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+if [ -e /usr/bin/time ]
+then
+    printf "\e[92m---Starting benchmarking ~100MB Image---\n\e[0m" \
+    && \
+    /usr/bin/time -f \
+    "Command that was run: %C
+    Elapsed (wall clock) time (m:ss): %Es
+    User time: %Us
+    System time: %Ss
+    Percent of CPU this job got: %P
+    Voluntary context switches: %W
+    Involuntary context switches: %c
+    Major (requiring I/O) page faults: %F
+    Minor (reclaiming a frame) page faults: %R
+    Page size (bytes): %Z" \
+    ./histogram-seq histogram_datafiles/small.bmp \
+    && \
+    printf "\n" \
+    && \
+    printf "\e[92m---Starting benchmarking ~400MB Image---\n\e[0m" \
+    && \
+    /usr/bin/time -f \
+    "Command that was run: %C
+    Elapsed (wall clock) time (m:ss): %Es
+    User time: %Us
+    System time: %Ss
+    Percent of CPU this job got: %P
+    Voluntary context switches: %W
+    Involuntary context switches: %c
+    Major (requiring I/O) page faults: %F
+    Minor (reclaiming a frame) page faults: %R
+    Page size (bytes): %Z" \
+    ./histogram-seq histogram_datafiles/med.bmp \
+    && \
+    printf "\n" \
+    &&
+    printf "\e[92m---Starting benchmarking ~1.4GB Image---\n\e[0m" \
+    && \
+    /usr/bin/time -f \
+    "Command that was run: %C
+    Elapsed (wall clock) time (m:ss): %Es
+    User time: %Us
+    System time: %Ss
+    Percent of CPU this job got: %P
+    Voluntary context switches: %W
+    Involuntary context switches: %c
+    Major (requiring I/O) page faults: %F
+    Minor (reclaiming a frame) page faults: %R
+    Page size (bytes): %Z" \
+    ./histogram-seq histogram_datafiles/large.bmp
+
+else
+    echo "You need to have TIME(1) in /usr/bin/time"
+fi
---
+++ assignment_2/tests/kmeans/run_pthread.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+if [ -e /usr/bin/time ]
+then
+    printf "\e[92m---Starting benchmarking d: 4096, c: 2048. p: 2048---\n\e[0m" \
+    && \
+    /usr/bin/time -f \
+    "Command that was run: %C
+    Elapsed (wall clock) time (m:ss): %Es
+    User time: %Us
+    System time: %Ss
+    Percent of CPU this job got: %P
+    Voluntary context switches: %W
+    Involuntary context switches: %c
+    Major (requiring I/O) page faults: %F
+    Minor (reclaiming a frame) page faults: %R
+    Page size (bytes): %Z" \
+    ./kmeans-pthread -d 4096 -c 2048 -p 2048 -s 256 \
+    && \
+    printf "\n" \
+    && \
+    printf "\e[92m---Starting benchmarking d: 4096, c: 4096, p: 2048---\n\e[0m" \
+    && \
+    /usr/bin/time -f \
+    "Command that was run: %C
+    Elapsed (wall clock) time (m:ss): %Es
+    User time: %Us
+    System time: %Ss
+    Percent of CPU this job got: %P
+    Voluntary context switches: %W
+    Involuntary context switches: %c
+    Major (requiring I/O) page faults: %F
+    Minor (reclaiming a frame) page faults: %R
+    Page size (bytes): %Z" \
+    ./kmeans-pthread -d 4096 -c 4096 -p 2048 -s 256 \
+    && \
+    printf "\n" \
+    &&
+    printf "\e[92m---Starting benchmarking d: 4096, c: 4096, p: 4096---\n\e[0m" \
+    && \
+    /usr/bin/time -f \
+    "Command that was run: %C
+    Elapsed (wall clock) time (m:ss): %Es
+    User time: %Us
+    System time: %Ss
+    Percent of CPU this job got: %P
+    Voluntary context switches: %W
+    Involuntary context switches: %c
+    Major (requiring I/O) page faults: %F
+    Minor (reclaiming a frame) page faults: %R
+    Page size (bytes): %Z" \
+    ./kmeans-pthread -d 4096 -c 4096 -p 4096 -s 256
+
+else
+    echo "You need to have TIME(1) in /usr/bin/time"
+fi
